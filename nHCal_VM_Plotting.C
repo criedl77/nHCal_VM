de27e4ff5167d45795b56a16fd1de91ab5baf34d
Caroline Riedl <criedl@illinois.edu>
2024-12-06 14:17:26 -0600

    tests

--- nHCal_VM_Plotting.C
+++ nHCal_VM_Plotting.C
@@ -46,9 +46,9 @@ void nHCal_VM_Plotting(){
   //TString strang = "podio_output_100events";
   //TString strang = "pythia8NCDIS_18x275_minQ2=1_beamEffects_xAngle=-0.025_hiDiv_1_100files";   
 
-  //TString strang = "Sartre_Au_phi_10runs";
+  TString strang = "Sartre_Au_phi_10runs";
 
-  TString strang = "pythia8NCDIS_18x275_minQ2=1_beamEffects_xAngle=-0.025_hiDiv_1";
+  //TString strang = "pythia8NCDIS_18x275_minQ2=1_beamEffects_xAngle=-0.025_hiDiv_1_1000runs";
   
   if (!strang.IsNull()) {
     // Directory does not exist (): try to make it - this doesn't work as it should...
