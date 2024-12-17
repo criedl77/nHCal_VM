cc41c926c4fd61c67911a71caba1aba896c05a61
Caroline Riedl <criedl@illinois.edu>
2024-12-17 15:45:00 -0600

    new j/psi MC

--- nHCal_VM_Plotting.C
+++ nHCal_VM_Plotting.C
@@ -52,9 +52,10 @@ void nHCal_VM_Plotting(){
   //TString strang = "Sartre_Au_phi_10runs";
 
   //TString strang = "DIFFRACTIVE_JPSI_ABCONV_18x275";
-  TString strang = "DIFFRACTIVE_JPSI_ABCONV_10x100";
-  //TString strang = "DIFFRACTIVE_JPSI_ABCONV_5x100";
-
+  //TString strang = "DIFFRACTIVE_JPSI_ABCONV_10x100";
+  TString strang = "DIFFRACTIVE_JPSI_ABCONV_5x100";
+  //TString strang = "DIFFRACTIVE_JPSI_ABCONV_5x41";
+  
   //TString strang = "pythia8NCDIS_18x275_minQ2=1_beamEffects_xAngle=-0.025_hiDiv_1_10files";
   //TString strang = "pythia8NCDIS_18x275_minQ2=1_beamEffects_xAngle=-0.025_hiDiv_1_1000runs";
   //TString strang = "pythia_ep_noradcor_18x275_q2_0.000000001_1.0_run39_10runs";
