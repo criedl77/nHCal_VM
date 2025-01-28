0a5d3c93b402356f1c0fd5784608564d0943d92c
Caroline Riedl <criedl@illinois.edu>
2025-01-28 16:14:29 -0600

    count 0, 1, 2  kaons

--- nHCal_VM_Analysis.C
+++ nHCal_VM_Analysis.C
@@ -856,9 +856,9 @@ void nHCal_VM_Analysis(){
   cout << "Number of generated phi: " << ngen_phi <<", of which decay into K+ K-: " << ndecay_phi_kk << " \n";
   cout << "         Of these " << 2*ndecay_phi_kk << " decay K+ K-, " << ndecay_phi_kaonpm_rec << " are reconstructed by ePIC (fraction " << fraction_phi_kaonpm_rec << "), and \n ";
   cout << "        " << ndecay_phi_kaonpm_nHCal << " reconstructed K+ K- make it into the nHCal acceptance, with corresponds to a fraction (of generated decay K+ K-) " << fraction_phi_kaonpm_nHCal << " \n";
-  cout << "  --> 0 kaons:" decay_phi_kaonpm_0_nHCal << "\n";
-  cout << "  --> 1 kaons:" decay_phi_kaonpm_1_nHCal << "\n";
-  cout << "  --> 2 kaons:" decay_phi_kaonpm_2_nHCal << "\n";
+  cout << "  --> 0 kaons:" << decay_phi_kaonpm_0_nHCal << "\n";
+  cout << "  --> 1 kaons:" << decay_phi_kaonpm_1_nHCal << "\n";
+  cout << "  --> 2 kaons:" << decay_phi_kaonpm_2_nHCal << "\n";
   cout << "Number of generated omega: " << ngen_omega << " \n";
   cout << "Number of generated J/Psi: " << ngen_jpsi << " , of which decay into e+ e-: " << ndecay_jpsi_ee << ", into mu+ mu-: " << ndecay_jpsi_mumu << " \n";
   cout << "        " << ndecay_jpsi_epm_nHCal << " reconstructed e+ e- make it into the nHCal acceptance, with corresponds to a fraction " << fraction_jpsi_epm_nHCal << " \n";
