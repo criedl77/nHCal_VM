effc4d5de0eccd82f891959ba774652e8cbd4272
Caroline Riedl <criedl@illinois.edu>
2025-01-30 16:48:25 -0600

    count 0, 1, 2  kaons

--- nHCal_VM_Analysis.C
+++ nHCal_VM_Analysis.C
@@ -142,7 +142,7 @@ void nHCal_VM_Analysis(){
   // eta phi(1020)
   TH1D *phiEta = new TH1D("phiEta","Eta of thrown #phi(1020);#eta",120,-6.,6.);
   TH1D *kpmfromphiEta = new TH1D("kpmfromphiEta","generated #eta of K^{#pm} from #phi(1020) decay;#eta",120,-6.,6.);
-  TH1D *kpmfromphiRecEta = new TH1D("kpmfromphiRecEta","reconstructed #eta of K^{#pm} from #phi(1020) decay;#eta",120,-7.,7.);
+  TH1D *kpmfromphiRecEta = new TH1D("kpmfromphiRecEta","reconstructed #eta of K^{#pm} from #phi(1020) decay;#eta",120,-6.,6.);
 
   TH1D *jpsiEta = new TH1D("jpsiEta","Eta of thrown J/#psi;#eta",120,-6.,6.);
   TH1D *epmfromjpsiEta = new TH1D("epmfromjpsiEta","generated #eta of e^{#pm} from J/#psi decay;#eta",120,-6.,6.);
@@ -799,7 +799,7 @@ void nHCal_VM_Analysis(){
 		    // check if K2 is any HCal acceptance:
 		    if( !calo_eta_acceptance("nhcal",recEta_phi_k2 ) && !calo_eta_acceptance("bhcal",recEta_phi_k2 ) && !calo_eta_acceptance("lfhcal",recEta_phi_k2 ) )
 		      {
-			cout << "*** reco K2 is in no HCal acceptance, eta = " << recEta_phi_k2  << ", reco index daughter-2: " << daughters_index[i_daughters_begin]+1 << " \n\n";
+			cout << "*** event " << ievgen << " - reco K2 is in no HCal acceptance, eta = " << recEta_phi_k2  << ", reco index daughter-2: " << daughters_index[i_daughters_begin]+1 << " \n\n";
 		      }
 		    
 		    
@@ -924,9 +924,9 @@ void nHCal_VM_Analysis(){
 	       cout << "*** WARNING: number of phi decay daughters larger than 2, " << n_this_decay_phi_kaonpm_k1_rec_nHCal +  n_this_decay_phi_kaonpm_k2_rec_nHCal << " \n";
 	    }
 
-	  // if( n_this_decay_phi_kaonpm_k1_rec_nHCal +  n_this_decay_phi_kaonpm_k2_rec_nHCal == 1 )						//														 {
-	    //cout << "***EVENT with 1 reco kaon in nHCal: " << n_this_decay_phi_kaonpm_k1_rec_nHCal +  n_this_decay_phi_kaonpm_k2_rec_nHCal << ", with the other counts - bHCal K1: " << n_this_decay_phi_kaonpm_k1_rec_bHCal << ", K2: " << n_this_decay_phi_kaonpm_k2_rec_bHCal << ", lfHCal K1: " << n_this_decay_phi_kaonpm_k1_rec_lfHCal << ", K2: " << n_this_decay_phi_kaonpm_k2_rec_lfHCal << "\n";
-	  //}
+	  if( n_this_decay_phi_kaonpm_k1_rec_nHCal +  n_this_decay_phi_kaonpm_k2_rec_nHCal == 1 )																				 {
+	    cout << "***EVENT " << ievgen << " with 1 reco kaon in nHCal: " << n_this_decay_phi_kaonpm_k1_rec_nHCal +  n_this_decay_phi_kaonpm_k2_rec_nHCal << ", with the other counts - bHCal K1: " << n_this_decay_phi_kaonpm_k1_rec_bHCal << ", K2: " << n_this_decay_phi_kaonpm_k2_rec_bHCal << ", lfHCal K1: " << n_this_decay_phi_kaonpm_k1_rec_lfHCal << ", K2: " << n_this_decay_phi_kaonpm_k2_rec_lfHCal << "\n";
+	  }
 																				       
 	    
 	} // end phiToKK
