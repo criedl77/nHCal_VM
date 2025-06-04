2b80c661a1c1f61642deb2480ab13dfefd7e166a
Caroline Riedl <criedl@illinois.edu>
2025-06-04 15:02:43 -0400

    make code flexible so it can skip the reconstructedChargedParticles

--- nHCal_VM_Analysis.C
+++ nHCal_VM_Analysis.C
@@ -4,7 +4,7 @@ void nHCal_VM_Analysis(int RecChaPa){
 //const char strang[]="podio_output"){
 
   gSystem->Exec("date");
-  int RecChaPar = 1; 
+  int RecChaPar = 0; 
   cout << "+ RecChaPar: " << RecChaPar << " \n";
   TString flavor = "nHCal_VM"; 
 
