7080e3a61ff4239f6580293eb003ac036bf6df1f
Caroline Riedl <criedl@illinois.edu>
2025-06-11 16:23:30 -0500

    expand cluster analysis

--- nHCal_VM_Analysis.C
+++ nHCal_VM_Analysis.C
@@ -883,10 +883,10 @@ void nHCal_VM_Analysis(int RecChaPar=1, int mode=1, TString strang = "sartre_bno
 	}// end of if(RecChaPar)
 
     // nHCal cluster associations to MCParticles:
-    cout << "+ MCParticle: " << i << " simuAssocClusters_nHCal.GetSize() " << simuAssocClusters_nHCal.GetSize() << " \n ";
+    //cout << "+ MCParticle: " << i << " simuAssocClusters_nHCal.GetSize() " << simuAssocClusters_nHCal.GetSize() << " \n ";
                 
     for(unsigned int k=0; k<simuAssocClusters_nHCal.GetSize(); k++) {
-        cout << "--- MCParticle: " << i << ", PDG: " << partPdg[i] << " nHCal      cluster: " << k << " \n";
+        //cout << "--- MCParticle: " << i << ", PDG: " << partPdg[i] << " nHCal      cluster: " << k << " \n";
         // Find association index matching the index of the MCParticle we are looking at (i):
         if(simuAssocClusters_nHCal[k] == i){
          //cout << " MCParticle: " << i << ", PDG: " << partPdg[i] << ", matching cluster ID in the nHCal: " << k << ", cluster energy: " << nHCalClustersE[recoAssocClusters_nHCal[k]] << ", cluster position X: " << nHCalClustersPosX[recoAssocClusters_nHCal[k]] <<  ", cluster position Y: " << nHCalClustersPosY[recoAssocClusters_nHCal[k]] <<  ", cluster position Z: " << nHCalClustersPosZ[recoAssocClusters_nHCal[k]] << " \n";
@@ -894,7 +894,7 @@ void nHCal_VM_Analysis(int RecChaPar=1, int mode=1, TString strang = "sartre_bno
         nHCalClustersEnergy_muons->Fill(nHCalClustersE[recoAssocClusters_nHCal[k]]);
         nHCalClustersPosXY_muons->Fill(nHCalClustersPosX[recoAssocClusters_nHCal[k]],nHCalClustersPosY[recoAssocClusters_nHCal[k]]);
         nHCalClustersPosZX_muons->Fill(nHCalClustersPosZ[recoAssocClusters_nHCal[k]],nHCalClustersPosX[recoAssocClusters_nHCal[k]]);
-        nHCalClustersPosZY_muons->Fill(nHCalClustersPosY[recoAssocClusters_nHCal[k]],nHCalClustersPosY[recoAssocClusters_nHCal[k]]);
+        nHCalClustersPosZY_muons->Fill(nHCalClustersPosZ[recoAssocClusters_nHCal[k]],nHCalClustersPosY[recoAssocClusters_nHCal[k]]);
             
             
               }// end of matching cluster in nHCal
